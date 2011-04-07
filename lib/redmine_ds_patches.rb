214b4c108955f85a1adc88d8cb98f14715147cab
pic <piccinini@gmail.com>
2011-04-07 15:24:50 +0200

    used the 'Issue' name for all trackers

---
+++ lib/redmine_ds_patches.rb
@@ -0,0 +1,14 @@
+require_dependency 'timelog_helper'
+require_dependency 'tracker'
+
+module TimelogHelper
+  # to change format
+  #def format_date(d)
+  #end
+end
+
+class Tracker
+  def name
+    'Issue'
+  end
+end
